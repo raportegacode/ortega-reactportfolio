8bb5993f2eadf87054010d8b2cb689b86d46b8f8
raportegacode <raphaelmar.ortega@gmail.com>
2023-05-13 17:38:49 +0800

    navvvvvvvvvvvvvvvvvvvvv

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -1,4 +1,4 @@
-import React, { Component, useRef } from 'react'
+import React, { Component } from 'react'
 import './style/nav.css';
 import ClearLogo from './images/RapLogo-clear.png';
 import hamburger from './images/hamburger.png';
