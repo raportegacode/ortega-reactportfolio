4228c4f08f5110c31c964ab4ac1b0f7a0e3d22df
raportegacode <raphaelmar.ortega@gmail.com>
2023-05-13 17:35:35 +0800

    mobile nav

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -1,17 +1,57 @@
-import React, { Component } from 'react'
+import React, { Component, useRef } from 'react'
 import './style/nav.css';
 import ClearLogo from './images/RapLogo-clear.png';
-
+import hamburger from './images/hamburger.png';
+import anime from 'animejs/lib/anime.es.js';
 
 export class nav extends Component {
+
+
     render() {
+        // document.getElementById('nav-btn').onClick = slideIn.play;
+
+
+        function onClick() {
+
+
+            var x = document.getElementById("nav-cont");
+            if (x.style.display === "block") {
+                anime({
+                    targets: '.nav-cont',
+                    translateY: -550,
+
+                    // backgroundColor: '#FFF',
+                    duration: 800
+                });
+                setTimeout(() => {
+
+                    x.style.display = "none";
+                }, 800);
+            } else {
+                anime({
+                    targets: '.nav-cont',
+                    translateY: 1,
+
+                    duration: 800
+                });
+                x.style.display = "block";
+            }
+        }
         return (
             <div className='nav'>
                 <div className='fixedNav'>
-                    <li>
-                        <img className='navlogo' src={ClearLogo} alt='logo' />
-                    </li>
-                    <ul className='nav-cont'>
+                    <ul className='navlogo-cont'>
+
+                        <li>
+                            <img className='navlogo' src={ClearLogo} alt='logo' />
+                        </li>
+                        <li className='mobile-btn'>
+                            <a id='nav-btn' href="javascript:void(0);" onClick={onClick}>
+                                <img src={hamburger} alt="hamburger" />
+                            </a>
+                        </li>
+                    </ul>
+                    <ul className='nav-cont' id='nav-cont'>
                         <li>
                             <a href='#home'>
                                 Home
@@ -33,8 +73,11 @@ export class nav extends Component {
                             </a>
                         </li>
                     </ul>
+
                 </div>
             </div>
+
+
         )
     }
 }
