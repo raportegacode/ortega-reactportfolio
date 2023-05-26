54b433cf7be15de1de4d7edc2eeb4e264c797ab6
raportegacode <raphaelmar.ortega@gmail.com>
2023-05-26 13:54:26 +0800

    contact me v2

--- src/contact.jsx
+++ src/contact.jsx
@@ -15,34 +15,34 @@ function App() {
             <div className='contact-pageCont'>
                 <h1>Contact me</h1>
                 <p>Get in touch with me!</p>
-                <div className="contact-cont">
-                    <div className="column1">
-                        <div className='contact-details'>
-                            <img src={loc} alt="Pin Icon" />
-                            <p>Taytay, Rizal</p>
-                        </div>
-                        <a href='tel:09053248135' className='contact-details'>
-                            <img src={phone} alt="Pin Icon" />
-                            <p>09053248135</p>
-                        </a>
-                        <a href='mailto:raphaelmar.ortega@gmail.com' className='contact-details'>
-                            <img src={email} alt="Pin Icon" />
-                            <p>raphaelmar.ortega@gmail.com</p>
-                        </a>
-                        <a href='https://github.com/raportegacode' className='contact-details'>
-                            <img src={github} alt="Pin Icon" />
-                            <p>Go to my Github</p>
-                        </a>
-                        <a href='linkedin.com/in/raphael-ortega279836240' className='contact-details'>
-                            <img src={linkedin} alt="Pin Icon" />
-                            <p>Go to my Linkedin</p>
-                        </a>
+                {/* <div className="contact-cont"> */}
+                <div className="column1">
+                    <div className='contact-details'>
+                        <img src={loc} alt="Pin Icon" />
+                        <p>Taytay, Rizal</p>
                     </div>
-
-
+                    <a href='tel:09053248135' className='contact-details'>
+                        <img src={phone} alt="Pin Icon" />
+                        <p>09053248135</p>
+                    </a>
+                    <a href='mailto:raphaelmar.ortega@gmail.com' className='contact-details'>
+                        <img src={email} alt="Pin Icon" />
+                        <p>raphaelmar.ortega@gmail.com</p>
+                    </a>
+                    <a href='https://github.com/raportegacode' className='contact-details'>
+                        <img src={github} alt="Pin Icon" />
+                        <p>Go to my Github</p>
+                    </a>
+                    <a href='linkedin.com/in/raphael-ortega279836240' className='contact-details'>
+                        <img src={linkedin} alt="Pin Icon" />
+                        <p>Go to my Linkedin</p>
+                    </a>
                 </div>
+
+
             </div>
         </div>
+        // </div>
     );
 }
 
